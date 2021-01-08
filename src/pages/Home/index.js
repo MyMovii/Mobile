5137f30a52af396e400d30fcc29f9b05db7aaf70
guilhermeabel99 <gpeuvis@gmail.com>
2021-01-07 23:35:38 -0300

    fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,7 +14,7 @@ import Arrow from '../../assets/vector/arrow.png';
 const Home = () => {
     const navigation = useNavigation();
     function handleNavigate() {
-        navigation.navigate('/pages')
+        navigation.navigate('')
         navigation.navigate('Recommendations');
         navigation.navigate('Login');
     }
