0a5bba0a9f0d7c81cf93b2250716ef0757df016f
Guilherme  Abel da Silva <guilherme.silva@hiplatform.com>
2021-01-18 18:08:26 -0300

    oi

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,6 +12,7 @@ import Arrow from '../../assets/vector/arrow.png';
 
 
 const Home = () => {
+    
     const navigation = useNavigation();
     function handleNavigate() {
         navigation.navigate('');
